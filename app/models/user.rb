b3267408af47336335dcaaeaa1b63c813f205fc6
vitaly <realnatisk@gmail.com>
2019-07-19 14:26:07 +0300

    setup devise, user, letter_opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable,  and :omniauthable
+  devise :database_authenticatable, :confirmable, :recoverable, :registerable, :rememberable, :trackable, :timeoutable,
+         :validatable
 end
