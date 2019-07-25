a2adfd32cd92f99b2f129f7b3de911634e1f08f2
vitaly <realnatisk@gmail.com>
2019-07-25 17:26:31 +0300

    comment out smtp settings to check mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,11 +93,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default charset: "utf-8"
-  config.action_mailer.asset_host = 'https://dev-explorer.herokuapp.com'
-  config.action_mailer.default_url_options = { host: 'dev-explorer.herokuapp.com', protocol: 'https' }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default charset: "utf-8"
+  # config.action_mailer.asset_host = 'https://dev-explorer.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: 'dev-explorer.herokuapp.com', protocol: 'https' }
 
   config.action_mailer.smtp_settings = {
       user_name: Explorer.credentials[:smtp_user_name],
