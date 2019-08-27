103fed801b141698ae4b8e4fadfc503f8cb814c2
vitaly <realnatisk@gmail.com>
2019-08-27 14:57:34 +0300

    unsafe inline for script-src for permit inline script of google maps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https, 'https://maps.googleapis.com'
+    policy.script_src  :self, :https, :unsafe_inline
     policy.style_src   :self, :https, :unsafe_inline
     policy.connect_src :self
     policy.report_uri  ""
