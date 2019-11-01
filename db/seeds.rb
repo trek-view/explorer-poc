0d18698db9fde3da4024db44514ab6cdc64f09b1
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-01 15:43:18 -0400

    fixed seed error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,28 @@ unless User.first
 
   user.tours.create!(name: 'England tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   country_name: 'England',
+                  #  country_name: 'England',
                    tag_names: 'cheese, vine, fog',
                    tourer_tour_id: '11',
                    tour_type: 'water')
 
   user.tours.create!(name: 'Holland tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   country_name: 'England',
+                  #  country_name: 'England',
                    tag_names: 'joy, fun, fog',
                    tourer_tour_id: '12',
                    tour_type: 'air')
 
   user.tours.create!(name: 'Germany tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   country_name: 'Germany',
+                  #  country_name: 'Germany',
                    tag_names: 'auto, grass, trees',
                    tourer_tour_id: '13',
                    tour_type: 'air')
 
   user.tours.create!(name: 'West tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   country_name: 'Italy',
+                  #  country_name: 'Italy',
                    tag_names: 'leather, boots, vine',
                    tourer_tour_id: '14',
                    tour_type: 'land')
