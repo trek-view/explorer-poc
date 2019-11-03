18562f788a4f1787202609a1a209e6ab67bc11e9
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-03 12:34:02 -0500

    fixed tour table issue and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 unless User.first
+  country1 = Country.create!(name: 'United Kingdom', code: 'GB');
+  country2 = Country.create!(name: 'German', code: 'DE');
+
+
   user = User.new(name: 'Johnny', email: 'test_user@example.com', password: 'password', password_confirmation: 'password')
   user.skip_confirmation!
   user.save!
@@ -6,24 +10,28 @@ unless User.first
   user.tours.create!(name: 'England tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
                    tag_names: 'cheese, vine, fog',
+                   country_id: 1,
                    tourer_tour_id: '11',
                    tour_type: 'water')
 
   user.tours.create!(name: 'Holland tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
                    tag_names: 'joy, fun, fog',
+                   country_id: 1,
                    tourer_tour_id: '12',
                    tour_type: 'air')
 
   user.tours.create!(name: 'Germany tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
                    tag_names: 'auto, grass, trees',
+                   country_id: 2,
                    tourer_tour_id: '13',
                    tour_type: 'air')
 
   user.tours.create!(name: 'West tour',
                    description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
                    tag_names: 'leather, boots, vine',
+                   country_id: 1,
                    tourer_tour_id: '14',
                    tour_type: 'land')
 end
