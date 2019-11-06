993069bc56765eb5fab2f606e95e87724ec67881
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-06 09:25:06 -0500

    updated /api/v1/users/account_info response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,14 @@
 module Api::V1
   class UsersController < BaseController
     def account_info
-      user = {
-        :id => @api_user.id,
-        :name =>@api_user.name,
-        :email => @api_user.email
+      resp = {
+        account_info: {
+          id: @api_user.id,
+          name: @api_user.name,
+          email: @api_user.email
+        }
       }
-      render json: user, status: :ok
+      render json: resp, status: :ok
     end
   end
 end
