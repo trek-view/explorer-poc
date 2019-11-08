dd7c3b4a16edacfaf319a2299f0656a270a51d9a
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-07 21:31:39 -0500

    set staging env

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -2,4 +2,4 @@ Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY']
 Gibbon::Request.timeout = 15
 Gibbon::Request.open_timeout = 15
 Gibbon::Request.symbolize_keys = true
-Gibbon::Request.debug = false
+Gibbon::Request.debug = true
