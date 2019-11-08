dcf944497b37c146ee1445a5c48d8c1d27d11a84
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-08 09:07:56 -0500

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,37 @@
-unless User.first || Rails.env.production?
-  user = User.new(name: 'Johnny', email: 'test_user@example.com', password: 'password', password_confirmation: 'password')
-  user.skip_confirmation!
-  user.save!
+unless Rails.env.production?
+  unless User.first
+    user = User.new(name: 'Johnny', email: 'test_user@example.com', password: 'password', password_confirmation: 'password')
+      user.skip_confirmation!
+      user.save!
 
-  user.tours.create!(name: 'England tour',
-                   description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   tag_names: 'cheese, vine, fog',
-                   countries: 'GB, US',
-                   tourer_tour_id: '11',
-                   tour_type: 'water')
+      user.tours.create!(name: 'England tour',
+                      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
+                      tag_names: 'cheese, vine, fog',
+                      countries: 'GB, US',
+                      tourer_tour_id: '11',
+                      tour_type: 'water')
 
-  user.tours.create!(name: 'Holland tour',
-                   description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   tag_names: 'joy, fun, fog',
-                   countries: 'GB, US, GE',
-                   tourer_tour_id: '12',
-                   tour_type: 'air')
+      user.tours.create!(name: 'Holland tour',
+                      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
+                      tag_names: 'joy, fun, fog',
+                      countries: 'GB, US, GE',
+                      tourer_tour_id: '12',
+                      tour_type: 'air')
 
-  user.tours.create!(name: 'Germany tour',
-                   description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   tag_names: 'auto, grass, trees',
-                   countries: 'GB, US',
-                   tourer_tour_id: '13',
-                   tour_type: 'air')
+      user.tours.create!(name: 'Germany tour',
+                      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
+                      tag_names: 'auto, grass, trees',
+                      countries: 'GB, US',
+                      tourer_tour_id: '13',
+                      tour_type: 'air')
 
-  user.tours.create!(name: 'West tour',
-                   description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
-                   tag_names: 'leather, boots, vine',
-                   tourer_tour_id: '14',
-                   countries: 'GE',
-                   tour_type: 'land')
+      user.tours.create!(name: 'West tour',
+                      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.',
+                      tag_names: 'leather, boots, vine',
+                      tourer_tour_id: '14',
+                      countries: 'GE',
+                      tour_type: 'land')
+  end
 end
 
 AdminUser.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'])
