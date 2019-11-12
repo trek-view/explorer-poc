c2e003e271d7104e71f3ca2aed0edf5cb3d7899f
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-12 20:13:30 +0200

    rspec for signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  
+  describe "Spec for Sign Up" do
+    it "should create new user account" do
+      visit new_user_registration_path
+      email = "test1@gmail.com"
+      fill_in 'user_name', :with => "abcd"
+      fill_in 'user_email', :with => email
+      fill_in 'user_password', :with => "password"
+      fill_in 'user_password_confirmation', :with => "password"
+      check 'user_terms'
+      check 'user_global_subscribe'
+      click_button 'Sign up'
+      expect(page).to have_content  "a confirmation link has been sent to your email address"
+    end
+  end
 
+  describe "Delete user" do
+    it "should delete user" do
+      
+    end
+  end
 end
