c4e05d17bc9889b66020999ae7f4209439cf33e6
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-11-18 00:00:20 +0200

    rspec - GET /api/v1/tours

--- spec/controllers/api/v1/tours_controller_spec.rb
+++ spec/controllers/api/v1/tours_controller_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
+require Rails.root.join('spec', 'controllers', 'api', 'v1', 'shared_examples', 'respond_to_empty.rb')
 
-describe Api::V1::ToursController, :type => :request do
+describe Api::V1::ToursController, :type => :api do
   let(:user) { create :user }
   let(:tours) { create_list(:tour, 10) }
 
-  before(:all) do
-    header 'api-key', user.api_token
-  end
-
   describe 'GET /api/v1/tours' do
-    before do
-      get '/api/v1/tours'
+    context 'when call without query params' do
+      before do
+        header 'api-key', user.api_token
+        get '/api/v1/tours'
+      end
+
+      it 'returns tours' do
+        expect(json).not_to be_empty
+        expect(json['tours']).not_to be_empty
+        expect(json['_metadata']).not_to be_empty
+      end
     end
 
-    it 'returns tours' do
-      expect(json).not_to be_empty
-      expect(json['tours']).not_to be_empty
+    context 'when call with query params' do
+      it_behaves_like "respond to empty", '/api/v1/tours?country=xxx'
+      it_behaves_like "respond to empty", '/api/v1/tours?tag=xxx'
+      it_behaves_like "respond to empty", '/api/v1/tours?user_id=-1'
     end
   end
 
