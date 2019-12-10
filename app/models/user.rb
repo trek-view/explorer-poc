76146510cd51f9a20129b5e7c8229c643e763fc0
Artyom Ivanov <artyomivanov7788@gmail.com>
2019-12-10 04:39:08 +0200

    Usernames to be > 4 < 15 chars and passwords > 6 < 25 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,10 @@ class User < ApplicationRecord
                       message: 'should not contain whitespaces or special characters'
   validates_acceptance_of :terms
 
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :name, length: { minimum: 5, maximum: 16 }
+
   has_secure_token :api_token
   friendly_id :name, use: :slugged
   acts_as_favoritor
