a4e5907de1797d283a49828b613af4129fb1106b
davidg <commits@himynamesdave.com>
2020-01-27 18:05:35 +0000

    Revert "Update photo.rb"
    This reverts commit 5c0f04c3256739b52e2fce49d34254d9c29bb360.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,8 +8,9 @@ class Photo < ApplicationRecord
   belongs_to :tour, counter_cache: true
   belongs_to :country
 
-  store_accessor :address, :locality, :administrative_area_level_3, :administrative_area_level_2, :administrative_area_level_1, :postal_code, :country, :country_code, :place_id, :plus_code
-  store_accessor :streetview, :capture_time, :share_link, :download_url, :thumbnail_url, :lat, :lon, :altitude, :heading, :pitch, :roll, :level, :connections
+  store_accessor :address, :cafe, :road, :suburb, :county, :region, :state, :postcode, :country_code
+  store_accessor :google, :plus_code_global_code, :plus_code_compound_code
+  store_accessor :streetview,  :capture_time, :share_link, :download_url, :thumbnail_url, :lat, :lon, :altitude, :heading, :pitch, :roll, :level, :connections
   store_accessor :tourer, :connections
   store_accessor :opentrailview, :photo_id
 
@@ -23,7 +24,8 @@ class Photo < ApplicationRecord
   validates :camera_model, length: { maximum: 255 }
   validates :country, presence: true
   validates :country_code, presence: true
-  validates :plus_code, length: { maximum: 255 }
+  validates :plus_code_global_code, length: { maximum: 255 }
+  validates :plus_code_compound_code, length: { maximum: 255 }
   validates :lat, numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }, length: { maximum: 20 }, allow_blank: true
   validates :lon, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, length: { maximum: 20 }, allow_blank: true
   validates :heading, numericality: { greater_than_or_equal_to:  0, less_than_or_equal_to:  360 }, allow_blank: true
